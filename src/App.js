3b8877572c0254e331536f507d4501b352a84975
leesol <jbkljo@naver.com>
2022-08-16 12:11:07 +0900

    선택바 만들기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import Length from "./Length";
 import Time from "./Time";
 import Volume from "./Volume";
 
 function App() {
+  const [index, setIndex] = useState("0");
+  const onSelect = (e) => {
+    setIndex(e.target.value);
+  };
   return (
     <div className="App">
-      <Time />
-      <Length />
-      <Volume />
+      <h3>단위변환 계산기</h3>;
+      <select value={index} onChange={onSelect}>
+        <option value={0}>분(m)&시간(h)변환</option>
+        <option value={1}>마일(mile)&킬로마일(km)변환</option>
+        <option value={2}>리터(l)&온스(oz)변환</option>
+      </select>
+      <hr />
+      {index === "0" ? <Time /> : null}
+      {index === "1" ? <Length /> : null}
+      {index === "2" ? <Volume /> : null}
     </div>
   );
 }
